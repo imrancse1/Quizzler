fe58c3f0f99178b948b7cfe8d52b6d219fd9fe0d
Imran Hossain <54549205+imrancse1@users.noreply.github.com>
2019-12-22 01:57:15 -0800

    Add files via upload

---
+++ Quizzler-iOS13/Quizzler-iOS13/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Zakaria on 3/12/19.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: String
+}
---
+++ Quizzler-iOS13/Quizzler-iOS13/ViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  Quizzler-iOS13
+//
+//  Created by Angela Yu on 12/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    //@IBOutlet weak var questionLabel: UILabel!
+    
+    //@IBOutlet weak var progressBar: UIProgressView!
+    
+    //@IBOutlet weak var trueButton: UIButton!
+    //@IBOutlet weak var falseButton: UIButton!
+    
+//    let quiz = [
+//        Question(text: "Four + Two is equal to six.", answer: "True"),
+//        Question(text: "Five - Three is greater than One.", answer: "True"),
+//        Question(text: "Three + Eight is less than Ten.", answer: "False"),
+//    ]
+//    var questionNumber = 0
+//    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       //questionLabel.text = "Four + Two is equal to six."
+       //updateUI()
+   }
+//
+//
+//    @IBAction func answerButtonPressed(_ sender: UIButton) {
+//        let userAnswer = sender.currentTitle
+//        let actualQuestion = quiz[questionNumber]
+//        let actualAnsweer = actualQuestion.answer
+//        
+//        if userAnswer == actualAnsweer {
+//            print("Right!")
+//        }else {
+//            print("Worng!")
+//        }
+//        if questionNumber + 1 < quiz.count{
+//            questionNumber += 1
+//        }else {
+//            questionNumber = 0
+//        }
+//        
+//        updateUI()
+//    }
+//    
+//    func updateUI(){
+//        questionLabel.text = quiz[questionNumber].text
+//    }
+}
+
